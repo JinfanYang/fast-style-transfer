2214c3197132de29ad6d7ecf3291ec75d62888f3
Jinfan Yang <yangjinfan22@gmail.com>
2019-04-11 14:17:31 -0700

    update data.py

--- data.py
+++ data.py
@@ -2,12 +2,10 @@ from __future__ import print_function
 import zipfile
 import os
 
-train_zip = 'images/train2014.zip'
-    
-# extract train2014.zip to train_data
+train_zip = 'images/train2017.zip'
 train_folder = 'images/train_images'
 if not os.path.isdir(train_folder):
     print(train_folder + ' not found, extracting ' + train_zip)
     zip_ref = zipfile.ZipFile(train_zip, 'r')
-    zip_ref.extractall(folder)
+    zip_ref.extractall(train_folder)
     zip_ref.close()
